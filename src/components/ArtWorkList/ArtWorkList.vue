19940b53151e9746125afe2b765d614c397b1afa
William Silva <alves.williamsilva@gmail.com>
2024-04-24 14:05:51 +0200

    Add Search feature

--- src/components/ArtWorkList/ArtWorkList.vue
+++ src/components/ArtWorkList/ArtWorkList.vue
@@ -3,11 +3,13 @@ import { onMounted, ref, watch } from "vue";
 import axios from "axios";
 import ArtWorkCard from "@/components/ArtWorkCard/ArtWorkCard.vue";
 import LoadingSkeleton from "@/components/ui/LoadingSkeleton.vue";
+import SearchInput from "@/components/SearchInput/SearchInput.vue";
 
 const artWorks = ref([]);
 const currentPage = ref(1);
 const isLoading = ref(false);
 const loadingMore = ref(false);
+const isEmpty = ref(false);
 
 const loadMore = () => {
   currentPage.value++;
@@ -29,6 +31,25 @@ const fetchArtwork = async () => {
   }
 };
 
+const searchArtwork = async (searchInput) => {
+  isLoading.value = true;
+  isEmpty.value = false;
+  try {
+    const response = await axios.get(
+      `https://www.rijksmuseum.nl/api/en/collection?key=xTQEfbFE&ps=12&p=${currentPage.value}&q=${searchInput.value}`
+    );
+    artWorks.value = response.data.artObjects;
+
+    if (!artWorks.value.length) {
+      isEmpty.value = true;
+    }
+  } catch (error) {
+    console.log("Error searching artworks: ", error);
+  } finally {
+    isLoading.value = false;
+  }
+};
+
 onMounted(() => {
   fetchArtwork();
 });
@@ -38,7 +59,9 @@ watch(currentPage, fetchArtwork);
 
 <template>
   <div>
+    <SearchInput v-on:search="searchArtwork" />
     <LoadingSkeleton v-if="isLoading" />
+    <p v-else-if="isEmpty">No Artwork was found.</p>
     <div v-else-if="artWorks.length" class="list-wrapper">
       <div class="artWorks-list">
         <ArtWorkCard v-for="artWork in artWorks" :key="artWork.id" :artWork="artWork" />
